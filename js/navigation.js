a956f92334f4590c74f43f342f4257d1973f032f
riquelima <riquelima@users.noreply.github.com>
2026-05-14 18:35:51 -0300

    up

--- js/navigation.js
+++ js/navigation.js
@@ -1,13 +1,4 @@
 window.goBack = function () {
-    var stack = JSON.parse(sessionStorage.getItem('mclNavStack') || '[]');
-    if (stack.length > 0) {
-        var prev = stack.pop();
-        sessionStorage.setItem('mclNavStack', JSON.stringify(stack));
-        if (prev && prev !== window.location.pathname.split('/').pop()) {
-            window.location.href = prev;
-            return;
-        }
-    }
     if (window.history.length > 1) {
         window.history.back();
     } else {
